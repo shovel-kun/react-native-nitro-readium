79bb87f6c06f56f3b16d1227657dd8093601659d
Ebisuzawa Kurumi <ebisuzawakurumi@proton.me>
2025-03-29 08:12:10 +0800

    fix: compilation error

--- android/src/main/java/com/nitroreadium/HybridPublication.kt
+++ android/src/main/java/com/nitroreadium/HybridPublication.kt
@@ -42,12 +42,10 @@ class HybridPublication(val publication: Publication) : HybridPublicationSpec() 
             return Promise.async { cacheFileUrl }
         }
 
-        publication.
-
-            // val baos = ByteArrayOutputStream()
-            // publication.cover()?.compress(Bitmap.CompressFormat.JPEG, 100, baos) ?: throw Exception("No cover found")
-            // val b = baos.toByteArray()
-            // Base64.encodeToString(b, Base64.DEFAULT)
+        // val baos = ByteArrayOutputStream()
+        // publication.cover()?.compress(Bitmap.CompressFormat.JPEG, 100, baos) ?: throw Exception("No cover found")
+        // val b = baos.toByteArray()
+        // Base64.encodeToString(b, Base64.DEFAULT)
 
         return Promise.async {
             FileOutputStream(cacheFile).use { fos ->
